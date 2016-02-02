d1ee7da0f7b020235e69492d904e57a7dbe536c6
shanikalakmali <shaniz.lakmali@gmail.com>
2016-02-02 12:40:45 +0530

    Added crud for wishlist

---
+++ models/Wishlist.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "wishlist".
+ *
+ * @property integer $id
+ * @property integer $user_id
+ * @property integer $imoji
+ * @property string $assigned_to
+ * @property string $date
+ *
+ * @property User $user
+ */
+class Wishlist extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'wishlist';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'imoji', 'assigned_to', 'date'], 'required'],
+            [['user_id', 'imoji'], 'integer'],
+            [['assigned_to'], 'string'],
+            [['date'], 'safe']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'imoji' => 'Imoji',
+            'assigned_to' => 'Assigned To',
+            'date' => 'Date',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+}
