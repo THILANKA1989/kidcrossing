d57d92462b55ac8d136d45a8aeb878ab71ade90b
Amal Ranganath <amal@livingdreams.lk>
2016-02-29 10:01:47 +0530

    SQL script Data directory updated

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=kid_crossing',
     'username' => 'root',
-    'password' => '',
+    'password' => 'root',
     'charset' => 'utf8',
 ];
